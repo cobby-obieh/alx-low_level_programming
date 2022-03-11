aabe656c9486cc41fe95397d0d58d080da229d5d
cobby-obieh <cobbyobieh@gmail.com>
2022-03-11 14:37:17 -0800

    Answer to 102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,15 +7,25 @@
  */
 int main(void)
 {
-	char a;
+	int i;
+	int j;
 
-	for (a = 'a'; a <= 'z' ; a++)
+	for (i = 0; i <= 99; i++)
 	{
-		if (a == 'q' || a == 'e')
+		for (j = i + 1; j <= 99; j++)
 		{
-			continue;
+			putchar(i / 10 + '0');
+			putchar(i % 10 + '0');
+			putchar(' ');
+			putchar(j / 10 + '0');
+			putchar(j % 10 + '0');
+
+			if (!(i == 98 && j == 99))
+			{
+				putchar(',');
+				putchar(' ');
+			}
 		}
-		putchar(a);
 	}
 	putchar('\n');
 	return (0);
